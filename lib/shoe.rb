f1ce1679aa31d65eb7a79b2c94ca6409e1b5a104
Noah <noahmwangi@gmail.com>
2023-05-23 08:02:02 +0300

    Completed Assignment

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,1 +1,13 @@
-# Make your shoe class here!
+Class Shoe
+attr_accessor :brand, :color, :size, :material, :condition
+
+def initialize(brand)
+    @brand = brand
+end
+
+def cobble
+    puts "the shoe has been repaired"
+    @condition = "new"
+end
+ 
+end
